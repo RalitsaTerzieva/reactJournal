7432d6b8430296e4f7dd6148bae80878ccb123c7
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2021-12-05 08:22:21 +0200

    Add google maps to about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,6 +2,7 @@ import Main from './Main';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 const About = () => {
     return (
@@ -16,6 +17,7 @@ const About = () => {
                backgroundColor: '#FFFAFA',
       }}>
           <Typography variant='h4' sx={{mb: 3}}>Our mission</Typography>
+          <Stack direction="row" spacing={4}>
             <Box sx={{textAlign: 'justify'}}>
             “Let food be the medicine and medicine be thy food.” – Hippocrates.
     This amazing quotes defines our mission and goal. Our Journal Application is created in November 2021 to your true friend that will help you building healthy daily routine. 
@@ -25,6 +27,10 @@ const About = () => {
     Firstly we started this journey with only one notebook and pencil. Secondly it become so bigger that all of my bookshelfs was full with notebooks with information about our lifestyle. We wrote down everything and we noticed so many things about us.
     When you start to write down your day, it becomes more and more clearer what is wrong and right for you and how are you feeling during every day. For many peaople this is a obligation but we don't think that health is just a obligation. Health is the path to everything in life.
             </Box>
+            <Box>
+                <iframe title="location" width="500" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Central%20park,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            </Box>
+            </Stack>
            </Paper>
        </Main>
     );
